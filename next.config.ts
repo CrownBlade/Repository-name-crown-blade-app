295a3ff6e3f3a45144865acbb2d0bed9bd76c213
Jhonathan <cjhonathan95@gmail.com>
2026-05-15 17:48:07 -0500

    Stable deploy without PWA

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: false,
-});
-
 const nextConfig = {
   allowedDevOrigins: ["192.168.1.72"],
 };
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
