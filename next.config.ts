d3475a72dc579c258093334c4a191cde668230aa
Jhonathan <cjhonathan95@gmail.com>
2026-05-15 17:37:40 -0500

    Actualización PWA y mejoras

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development"
+});
+
 const nextConfig = {
   allowedDevOrigins: ["192.168.1.72"],
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
